5d5330042ef1cbad659fa2cebbd6b2c6b7bf2c0f
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-19 02:54:41 -0400

    refactor: implement user repository methods with PostgreSQL

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,14 +1,68 @@
-import { IUser } from './users.interfaces'
-import User from './users.models'
+import { IPublicUserData, IUser } from './users.interfaces'
+import pool from '../../config/postgress.config'
 
 class UserRepository {
-  insertUser = (user: IUser) => new User(user).save()
-  getUsers = () => User.find()
-  getUser = (id: string) => User.findById(id)
-  deleteUser = (id: string) => User.findByIdAndDelete(id)
-  getByEmail = (email: string) => User.findOne({ email })
-  updateUser = (id: string, updatedUser: IUser) =>
-    User.findByIdAndUpdate(id, updatedUser, { new: true })
+  // insert a new user
+  insertUser = async (user: IUser): Promise<IPublicUserData> => {
+    const client = await pool.connect()
+    const result = await client.query<IPublicUserData>(
+      'INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) returning id, name, email, role',
+      [user.name, user.email, user.password, user.role]
+    )
+    console.log(result.rows)
+    client.release()
+    return result.rows[0]
+  }
+
+  // get all users
+  getUsers = async (): Promise<IPublicUserData[]> => {
+    const client = await pool.connect()
+    const result = await client.query<IPublicUserData>('SELECT * FROM users')
+    console.log(result.rows)
+    client.release()
+    return result.rows
+  }
+
+  // get a user by id
+  getUser = async (id: string): Promise<IPublicUserData> => {
+    const client = await pool.connect()
+    const result = await client.query<IPublicUserData>('SELECT * FROM users WHERE id = $1', [id])
+    console.log(result.rows)
+    client.release()
+    return result.rows[0]
+  }
+
+  // get a user by email
+  getByEmail = async (email: string): Promise<IPublicUserData> => {
+    const client = await pool.connect()
+    const result = await client.query<IPublicUserData>('SELECT * FROM users WHERE email = $1', [
+      email,
+    ])
+    console.log(result.rows)
+    client.release()
+    return result.rows[0]
+  }
+
+  // update a user
+  updateUser = async (id: string, updatedUser: IUser): Promise<IPublicUserData> => {
+    const client = await pool.connect()
+    const result = await client.query<IPublicUserData>(
+      'UPDATE users SET name = $1, email = $2, password = $3, role = $4 WHERE id = $5 returning id, name, email, role',
+      [updatedUser.name, updatedUser.email, updatedUser.password, updatedUser.role, id]
+    )
+    console.log(result.rows)
+    client.release()
+    return result.rows[0]
+  }
+
+  // delete a user by id
+  deleteUser = async (id: string): Promise<IPublicUserData> => {
+    const client = await pool.connect()
+    const result = await client.query<IPublicUserData>('DELETE FROM users WHERE id = $1', [id])
+    console.log(result.rows)
+    client.release()
+    return result.rows[0]
+  }
 }
 
 export default new UserRepository()
