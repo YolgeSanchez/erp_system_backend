18c63d875435babf2c8b514f48b4a48e35b302c8
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-19 08:02:49 -0400

    refactor: add some roles to the interfaces that manages those values

--- src/modules/users/users.interfaces.ts
+++ src/modules/users/users.interfaces.ts
@@ -2,14 +2,12 @@ export interface IPublicUserData {
   id: string
   name: string
   email: string
-  role: 'user' | 'admin'
-  // replace or add any field you want for your public user data
+  role: 'super' | 'admin' | 'inventory' | 'employee'
 }
 
 export interface IUser {
   name: string
   email: string
   password: string
-  role: 'user' | 'admin'
-  // replace or add any field you want for your user model interface
+  role: 'super' | 'admin' | 'inventory' | 'employee'
 }
