488a8a776deafcfe6077160630ab7482dcb35d46
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-19 02:52:44 -0400

    refactor: update server initialization to use PostgreSQL

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
-import db from './config/mongo'
+import pool from './config/postgress.config'
 import app from './app'
 import initAdminUser from './utils/adminUser.handle'
 
 // change this to the port you want to use
 const PORT = process.env.PORT || 3000
 
-// run the server
-db().then(() => {
-  console.log('>>> Connected to MongoDB database')
-  initAdminUser()
-  app.listen(PORT, () => console.log(`>>> Running on port ${PORT}`))
-})
+const startServer = async () => {
+  try {
+    await initAdminUser()
+    console.log('>>> Connected to the database')
+    app.listen(PORT, () => console.log(`>>> Running on port ${PORT}`))
+  } catch (error) {
+    console.log('Error connecting to the database:', error)
+  }
+}
+
+startServer()
