3cc0ccbe9eb43ff27b2a511218923bf0954dd0d1
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-19 20:26:29 -0400

    refactor(auth): update role validation schema

--- src/modules/users/users.schemas.ts
+++ src/modules/users/users.schemas.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod'
 
-enum Role {
-  user = 'user',
+enum Roles {
+  super = 'super',
   admin = 'admin',
+  inventory = 'inventory',
+  employee = 'employee',
 }
 
 export const registerSchema = z.object({
   name: z.string().min(2, 'name should be longer than or equal to 2 characters').max(50),
   email: z.string().email({ message: 'email should be a valid email' }),
   password: z.string().min(6, 'password should be at least 6 characters long'),
-  role: z.nativeEnum(Role),
+  role: z.nativeEnum(Roles),
 })
-
-// don't forget to modify here each thing you change in the auth.interfaces file
