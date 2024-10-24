1c02bf236caa8786b6a59659b86e1088221d6d78
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-24 02:12:15 -0400

    feat(routes): implement inventory routes with role-based authorization

--- src/modules/inventory/inventory.routes.ts
+++ src/modules/inventory/inventory.routes.ts
@@ -7,7 +7,27 @@ import { inventorySchema } from './inventory.schema'
 const router = Router()
 
 /**
- *
+ * [inventory routes]
+ * http://localhost:3001/api/inventory/products [GET]
+ * http://localhost:3001/api/inventory/products/:sku [GET]
+ * http://localhost:3001/api/inventory/products [POST]
+ * http://localhost:3001/api/inventory/products/:sku [PUT]
+ * http://localhost:3001/api/inventory/products/:sku [DELETE]
  */
 
+// anyone can access the products
+router.use(auth(['super', 'admin', 'manager', 'employee', 'inventory']))
+router.get('/products', InventoryController.getProducts)
+router.get('/products/:sku', InventoryController.getProductBySku)
+
+// only super, admin, manager, inventory can create and delete a product
+router.use(auth(['super', 'admin', 'inventory']))
+router.post('/products', validate(inventorySchema), InventoryController.insertProduct)
+router.delete('/products/:sku', InventoryController.deleteProduct)
+
+// only super, admin, manager, inventory can update a product
+router.use(auth(['super', 'admin', 'manager', 'inventory']))
+router.put('/products/:sku', validate(inventorySchema), InventoryController.updateProduct)
+
+// export the routes
 export default router
