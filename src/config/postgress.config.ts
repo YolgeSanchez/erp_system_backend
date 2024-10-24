a8e581729c89fd6c85d61f3a558383c6fb50be0b
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-24 02:31:53 -0400

    refactor(database): streamline the query process via query function to connect and release

--- src/config/postgress.config.ts
+++ src/config/postgress.config.ts
@@ -14,4 +14,23 @@ const pool = new Pool({
   maxUses: 7500,
 })
 
+export const query = async (text: string, params?: any) => {
+  // start a timer
+  const start = Date.now()
+
+  // initialize a client for the query
+  const client = await pool.connect()
+  const res = await client.query(text, params)
+
+  // finish the timer and log the query info
+  const duration = Date.now() - start
+  console.log('executed query', { text, duration, rows: res.rowCount })
+
+  // return the client to the pool after using it
+  client.release()
+
+  // return the query result
+  return res.rows
+}
+
 export default pool
