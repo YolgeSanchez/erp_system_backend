2527ec51c3bc4a480c8cbd0df938d366589faf48
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-24 02:41:47 -0400

    refactor(repositories): make use of the new function to streamline the query process of the repo files

--- src/modules/inventory/inventory.repository.ts
+++ src/modules/inventory/inventory.repository.ts
@@ -1,11 +1,10 @@
 import { IProduct, IProductData } from './inventory.interfaces'
-import pool from '../../config/postgress.config'
+import { query } from '../../config/postgress.config'
 
 class InventoryRepository {
   // insert a new product
   insertProduct = async (product: IProduct): Promise<IProductData> => {
-    const client = await pool.connect()
-    const result = await client.query<IProductData>(
+    const result = await query(
       'INSERT INTO products ( prd_name, prd_description, prd_category, prd_brand, prd_unit_of_measurement, prd_purchase_price, prd_selling_price, prd_current_stock, prd_expiration_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning sku, prd_name, prd_description, prd_category, prd_brand, prd_unit_of_measurement, prd_purchase_price, prd_selling_price, prd_current_stock, prd_expiration_date, created_at',
       [
         product.prd_name,
@@ -20,43 +19,30 @@ class InventoryRepository {
       ]
     )
 
-    client.release()
-    return result.rows[0]
+    return result[0]
   }
 
   // get all products
   getProducts = async (): Promise<IProductData[]> => {
-    const client = await pool.connect()
-    const result = await client.query<IProductData>('SELECT * FROM products')
-
-    client.release()
-    return result.rows
+    const result = await query('SELECT * FROM products')
+    return result
   }
 
   // get a product by sku
   getProductBySku = async (sku?: string): Promise<IProductData> => {
-    const client = await pool.connect()
-    const result = await client.query<IProductData>('SELECT * FROM products WHERE sku = $1', [sku])
-
-    client.release()
-    return result.rows[0]
+    const result = await query('SELECT * FROM products WHERE sku = $1', [sku])
+    return result[0]
   }
 
   // get a product by prd_name
   getProductByName = async (prd_name: string): Promise<IProductData> => {
-    const client = await pool.connect()
-    const result = await client.query<IProductData>('SELECT * FROM products WHERE prd_name = $1', [
-      prd_name,
-    ])
-
-    client.release()
-    return result.rows[0]
+    const result = await query('SELECT * FROM products WHERE prd_name = $1', [prd_name])
+    return result[0]
   }
 
   // update a product
   updateProduct = async (sku: string, updatedProduct: IProduct): Promise<IProductData> => {
-    const client = await pool.connect()
-    const result = await client.query<IProductData>(
+    const result = await query(
       'UPDATE products SET prd_name = $1, prd_description = $2, prd_category = $3, prd_brand = $4, prd_unit_of_measurement = $5, prd_purchase_price = $6, prd_selling_price = $7, prd_current_stock = $8, prd_expiration_date = $9 WHERE sku = $10 returning sku, prd_name, prd_description, prd_category, prd_brand, prd_unit_of_measurement, prd_purchase_price, prd_selling_price, prd_current_stock, prd_expiration_date, created_at',
       [
         updatedProduct.prd_name,
@@ -72,17 +58,13 @@ class InventoryRepository {
       ]
     )
 
-    client.release()
-    return result.rows[0]
+    return result[0]
   }
 
   // delete a product by sku
   deleteProduct = async (sku: string): Promise<IProductData> => {
-    const client = await pool.connect()
-    const result = await client.query<IProductData>('DELETE FROM products WHERE sku = $1', [sku])
-
-    client.release()
-    return result.rows[0]
+    const result = await query('DELETE FROM products WHERE sku = $1', [sku])
+    return result[0]
   }
 }
 
--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,67 +1,47 @@
 import { IPublicUserData, IUser } from './users.interfaces'
-import pool from '../../config/postgress.config'
+import { query } from '../../config/postgress.config'
 
 class UserRepository {
   // insert a new user
   insertUser = async (user: IUser): Promise<IPublicUserData> => {
-    const client = await pool.connect()
-    const result = await client.query<IPublicUserData>(
+    const result = await query(
       'INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) returning id, name, email, role',
       [user.name, user.email, user.password, user.role]
     )
-    console.log(result.rows)
-    client.release()
-    return result.rows[0]
+    return result[0]
   }
 
   // get all users
   getUsers = async (): Promise<IPublicUserData[]> => {
-    const client = await pool.connect()
-    const result = await client.query<IPublicUserData>('SELECT * FROM users')
-    console.log(result.rows)
-    client.release()
-    return result.rows
+    const result = await query('SELECT * FROM users')
+    return result
   }
 
   // get a user by id
   getUser = async (id: string): Promise<IPublicUserData> => {
-    const client = await pool.connect()
-    const result = await client.query<IPublicUserData>('SELECT * FROM users WHERE id = $1', [id])
-    console.log(result.rows)
-    client.release()
-    return result.rows[0]
+    const result = await query('SELECT * FROM users WHERE id = $1', [id])
+    return result[0]
   }
 
   // get a user by email
   getByEmail = async (email: string): Promise<IPublicUserData> => {
-    const client = await pool.connect()
-    const result = await client.query<IPublicUserData>('SELECT * FROM users WHERE email = $1', [
-      email,
-    ])
-    console.log(result.rows)
-    client.release()
-    return result.rows[0]
+    const result = await query('SELECT * FROM users WHERE email = $1', [email])
+    return result[0]
   }
 
   // update a user
   updateUser = async (id: string, updatedUser: IUser): Promise<IPublicUserData> => {
-    const client = await pool.connect()
-    const result = await client.query<IPublicUserData>(
+    const result = await query(
       'UPDATE users SET name = $1, email = $2, password = $3, role = $4 WHERE id = $5 returning id, name, email, role',
       [updatedUser.name, updatedUser.email, updatedUser.password, updatedUser.role, id]
     )
-    console.log(result.rows)
-    client.release()
-    return result.rows[0]
+    return result[0]
   }
 
   // delete a user by id
   deleteUser = async (id: string): Promise<IPublicUserData> => {
-    const client = await pool.connect()
-    const result = await client.query<IPublicUserData>('DELETE FROM users WHERE id = $1', [id])
-    console.log(result.rows)
-    client.release()
-    return result.rows[0]
+    const result = await query('DELETE FROM users WHERE id = $1', [id])
+    return result[0]
   }
 }
 
